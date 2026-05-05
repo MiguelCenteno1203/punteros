3d9c2c9f6a76d187cd69c4f41e2127891393b609
Laboratorio <laboratorio@udla-ec.int>
2026-05-05 11:39:06 -0500

    second commit

--- punteros.c
+++ punteros.c
@@ -4,4 +4,8 @@ int main(int argc, char const *argv[])
     int num=15, a;
     int *x;
 
+    x = &num;  // x se le asigna la dirección de num
+    a = *x;    // a se le asigna el valor de x
+    printf("%p %d" ,x,a);
+
 }
