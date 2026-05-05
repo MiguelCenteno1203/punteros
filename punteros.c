e2eff09d444b8dff24d331b19aac91968d59379b
Laboratorio <laboratorio@udla-ec.int>
2026-05-05 11:48:18 -0500

    Correción, salto de linea

--- punteros.c
+++ punteros.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int num=15, a;
+    int num = 15, a;
     int *x;
 
     x = &num;  // x se le asigna la dirección de num
     a = *x;    // a se le asigna el valor de x
-    printf("%p %d" ,x,a);
+    printf("%p %d\n" ,x,a);
 
     char letra = 'a', c;
     char *ptr;
